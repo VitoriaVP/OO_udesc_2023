8a1a3c314721b9bcab59ea70fcb710683ebe5a40
Vitoria Prado Valdameri <vitoria.valdameri22@edu.udesc.br>
2024-05-07 11:20:11 -0300

    Classe Pessoa e respectivo teste dentro do mesmo pacote

---
+++ src/domain/TestaPacote.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class TestaPacote {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+
+        p.nome = " Zé da Silva ";
+
+        System.out.printf("Nome: " + p.nome);
+
+    }
+}
